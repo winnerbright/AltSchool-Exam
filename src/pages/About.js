9a9d3cdd3d973168b3f9a09ad10356de48f1124d
Winner Bright <brightwinnifred@gmail.com>
2022-11-06 14:16:14 -0800

    second commit

--- src/pages/About.js
+++ src/pages/About.js
@@ -18,7 +18,7 @@ function About() {
         <h1> ABOUT US</h1>
         <p>
         Learner is an online learning community with thousands of lessons on subjects including 
-        illustration, design, photography, video, freelancing, and more for those who are creative
+        illustration, design, photography, video, freelancing, and lots more for those who are creative
          and curious. On Learner, users congregate to get inspiration and advance their creative processes
          At Learner, we've witnessed time and time again how the ostensibly straightforward act of creating 
          can be a catalyst for personal development, evolution, and discovery. We wish to encourage and 
